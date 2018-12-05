c72c85c8407292d691db76c645cccc99655c6503
mysfell <m_ysgift@qq.com>
2018-12-05 14:26:45 +0800

    新增Spring Boot启动

---
+++ src/main/java/org/waren/WarenApplication.java
@@ -0,0 +1,12 @@
+package org.waren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarenApplication.class, args);
+	}
+}
